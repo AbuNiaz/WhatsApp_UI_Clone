2249ca5b0ece7c9ff304cf2188e82ad1e45366d1
Abu Niaz <niaz35-507@diu.edu.bd>
2022-06-03 11:37:58 +0600

    Update shohan_chat.dart

--- lib/Chats/Chat Box/shohan_chat.dart
+++ lib/Chats/Chat Box/shohan_chat.dart
@@ -6,7 +6,24 @@ class ShohanChats extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Row(
+          children: const [
+            CircleAvatar(
+              radius: 20,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text("Shohan")
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.video_call)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+      ),
     );
   }
 }
