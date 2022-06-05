6c56bb75847ccf9b1cb0577128460c323919e113
Abu Niaz <niaz35-507@diu.edu.bd>
2022-06-05 21:42:04 +0600

    Update shohan_chat.dart

--- lib/Chats/Chat Box/shohan_chat.dart
+++ lib/Chats/Chat Box/shohan_chat.dart
@@ -6,87 +6,105 @@ class ShohanChats extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: const [
-              CircleAvatar(
-                radius: 20,
-              ),
-              SizedBox(
-                width: 5,
-              ),
-              Text("Shohan")
-            ],
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.video_call),
+      appBar: AppBar(
+        title: Row(
+          children: const [
+            CircleAvatar(
+              radius: 20,
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.call),
+            SizedBox(
+              width: 5,
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.more_vert),
-            )
+            Text("Shohan")
           ],
         ),
-        body: Container(
-          color: Colors.grey,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: ListView(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.video_call),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.call),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          )
+        ],
+      ),
+      body: Container(
+        color: Colors.grey,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 170,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(15),
+                        bottomRight: Radius.circular(15),
+                        bottomLeft: Radius.circular(15),
+                      ),
+                    ),
+                    child: const Center(
+                      child: Text('Hi..'),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
                       height: 50,
                       width: 170,
                       decoration: const BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(15),
+                          topLeft: Radius.circular(15),
                           bottomRight: Radius.circular(15),
                           bottomLeft: Radius.circular(15),
                         ),
                       ),
                       child: const Center(
-                        child: Text('Hi..'),
+                        child: Text("Hello!!"),
+                      )),
+                ],
+              ),
+              Container(
+                height: 20,
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextFormField(
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        hintText: '',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                const BorderSide(color: Colors.purpleAccent)),
                       ),
                     ),
+                    IconButton(onPressed: () {}, icon: const Icon(Icons.send))
                   ],
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Container(
-                        height: 50,
-                        width: 170,
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(15),
-                            bottomRight: Radius.circular(15),
-                            bottomLeft: Radius.circular(15),
-                          ),
-                        ),
-                        child: const Center(
-                          child: Text("Hello!!"),
-                        )),
-                  ],
-                ),
-              ],
-            ),
+              )
+            ],
           ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [],
-        ));
+      ),
+    );
   }
 }
