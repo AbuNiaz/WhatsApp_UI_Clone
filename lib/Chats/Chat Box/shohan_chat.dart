df3d778df08ff71b61b4837aa331f6a1576bfdf0
Abu Niaz <niaz35-507@diu.edu.bd>
2022-06-20 22:21:28 +0600

    Update shohan_chat.dart

--- lib/Chats/Chat Box/shohan_chat.dart
+++ lib/Chats/Chat Box/shohan_chat.dart
@@ -30,7 +30,7 @@ class ShohanChats extends StatelessWidget {
           IconButton(
             onPressed: () {},
             icon: const Icon(Icons.more_vert),
-          )
+          ),
         ],
       ),
       body: Container(
@@ -69,19 +69,20 @@ class ShohanChats extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     Container(
-                        height: 50,
-                        width: 170,
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(15),
-                            bottomRight: Radius.circular(15),
-                            bottomLeft: Radius.circular(15),
-                          ),
+                      height: 50,
+                      width: 170,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          bottomRight: Radius.circular(15),
+                          bottomLeft: Radius.circular(15),
                         ),
-                        child: const Center(
-                          child: Text("Hello!!"),
-                        )),
+                      ),
+                      child: const Center(
+                        child: Text("Hello!!"),
+                      ),
+                    ),
                   ],
                 ),
                 const SizedBox(
